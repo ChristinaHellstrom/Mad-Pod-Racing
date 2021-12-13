6c96baea726c0b918985d788b859046d3757718b
Christina <chellstr@iki.fi>
2021-12-13 23:49:40 +0200

    Bronze League - Refactoring
    - Moved code into a class to clean up the main function
    - Added slowing thrust according to angle
    - Continued testing with slowing when approaching a checkpoint

--- MadPodRacing.cpp
+++ MadPodRacing.cpp
@@ -16,23 +16,224 @@ float lerp(float a, float b, float f)
     return a + f * (b - a);
 }
 
-int main()
+class track
 {
-    int thrust = 100;
-    bool boosted = false;
-    int last_distance = 0;
-    float BREAK_DISTANCE = 800.0f;
-    int TARGET_DISTANCE = 500;
-    int CHECK_ANGLE = 5;
-    float PI = 3.14159265f;
-    bool apex_reached = false;
+private:
+    struct track_state
+    {
+        int x;
+        int y;
+        int next_checkpoint_x; // x position of the next check point
+        int next_checkpoint_y; // y position of the next check point
+        int next_checkpoint_dist; // distance to the next checkpoint
+        int next_checkpoint_angle; // angle between your pod orientation and the direction of the next checkpoint 
+    };
+
+private:
+    std::vector<std::pair<int, int>> m_checkpoints;
+    track_state m_current_state;
+    track_state m_previous_state;
+    int m_checkpoint_index;
     int last_point_x;
     int last_point_y;
     int current_point_x;
     int current_point_y;
-    std::vector<std::pair<int, int>> goals;
     int next_goal;
-    bool goals_collected = false;
+    bool m_first_round = true;
+    bool m_boosted = false;
+    int m_boost_index;
+
+    float BREAK_DISTANCE = 1500.0f;
+    float TARGET_DISTANCE = 500.0f;
+    float CHECK_ANGLE = 2.0f;
+    float PI = 3.14159265f;
+
+public:
+    void UpdateStatus(int x, int y, int next_checkpoint_x, int next_checkpoint_y, int next_checkpoint_dist, int next_checkpoint_angle)
+    {
+        m_previous_state = m_current_state;
+
+        m_current_state.x = x;
+        m_current_state.y = y;
+        m_current_state.next_checkpoint_x = next_checkpoint_x;
+        m_current_state.next_checkpoint_y = next_checkpoint_y;
+        m_current_state.next_checkpoint_dist = next_checkpoint_dist;
+        m_current_state.next_checkpoint_angle = next_checkpoint_angle;
+
+        SetCheckpoint();
+    }
+
+    bool IsNextCheckpoint() const
+    {
+        return m_checkpoints[m_checkpoint_index].first != m_current_state.next_checkpoint_x && m_checkpoints[m_checkpoint_index].second != m_current_state.next_checkpoint_y;
+    }
+
+    int Distance(const pair<int, int>& start, const pair<int, int>& end) const
+    {
+        int delta_x = (end.first - start.first);
+        int delta_y = (end.second - start.second);
+
+        // TODO: Change to use just square values, not squareroot for optimization
+        return sqrt(delta_x * delta_x + delta_y * delta_y);
+    }
+
+    int CalculateBoostIndex()
+    {
+        int max_distance = 0.0f;
+        int index;
+
+        for (int i = 0, max_i = m_checkpoints.size(); i < max_i; ++i)
+        {
+            int next_i = i + 1;
+            if (next_i == max_i)
+            {
+                next_i = 0;
+            }
+
+            int distance = Distance(m_checkpoints[i], m_checkpoints[next_i]);
+            if (distance > max_distance)
+            {
+                max_distance = distance;
+                index = next_i;
+            }
+        }
+
+        return index;
+    }
+
+    void SetCheckpoint()
+    {
+        if (m_checkpoints.empty())
+        {
+            m_checkpoints.push_back(pair<int, int>(m_current_state.next_checkpoint_x, m_current_state.next_checkpoint_y));
+            m_checkpoint_index = 0;
+        }
+        else
+        {
+            if (IsNextCheckpoint())
+            {
+                if (m_first_round)
+                {
+                    if (m_checkpoints[0].first == m_current_state.next_checkpoint_x && m_checkpoints[0].second == m_current_state.next_checkpoint_y)
+                    {
+                        m_first_round = false;
+                        m_checkpoint_index = 0;
+                        m_boost_index = CalculateBoostIndex();
+                    }
+                    else
+                    {
+                        m_checkpoints.push_back(pair<int, int>(m_current_state.next_checkpoint_x, m_current_state.next_checkpoint_y));
+                        ++m_checkpoint_index;
+                    }
+                }
+                else
+                {
+                    ++m_checkpoint_index;
+                    if (m_checkpoint_index >= m_checkpoints.size())
+                    {
+                        m_checkpoint_index = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    int GetPreviousCheckpointIndex()
+    {
+        int index = m_checkpoint_index - 1;
+        if (m_checkpoint_index < 0)
+        {
+            m_checkpoint_index = m_checkpoints.size() - 1;
+        }
+
+        return index;
+    }
+
+    string GetOutputString()
+    {
+        float delta_x = (m_current_state.next_checkpoint_x - m_current_state.x);
+        float delta_y = (m_current_state.next_checkpoint_y - m_current_state.y);
+
+        // float angle = atan (delta_y / delta_x);
+        // float distance = sqrt(delta_y * delta_y + delta_x * delta_x);
+
+        pair<int, int>& last_checkpoint = m_checkpoints[GetPreviousCheckpointIndex()];
+
+        float delta_lastpoint_x = (last_checkpoint.first - m_current_state.x);
+        float delta_lastpoint_y = (last_checkpoint.second - m_current_state.y);
+
+        float distance_last_point = sqrt(delta_lastpoint_y * delta_lastpoint_y + delta_lastpoint_x * delta_lastpoint_x);
+
+        float x_multiplier = delta_x < 0.0f ? -1 : 1;
+        float y_multiplier = delta_y < 0.0f ? -1 : 1;
+
+        //cerr << "angle: " <<  angle * 180.0f / PI << "  distance: " << distance << endl;
+        // cerr << "multipliers: (" << x_multiplier << ", " << y_multiplier << ")" << endl;
+        /*
+        float goal_distance = distance - TARGET_DISTANCE;
+        float dir_x = cos(angle) * goal_distance;
+        float dir_y = sin(angle) * goal_distance;
+        float goal_x = x + (x_multiplier) * abs(dir_x);
+        float goal_y = y + (y_multiplier) * abs(dir_y);
+        */
+
+        //cerr << "Goal: (" << goal_x << ", " << goal_y << "), dir: (" << dir_x << ", " << dir_y << ")" << endl;
+
+        int thrust = 100;
+
+        if (abs(m_current_state.next_checkpoint_angle) > 90)
+        {
+            cerr << "====STOP:" << endl;
+            thrust = 0;
+        }
+        else if (m_current_state.next_checkpoint_dist < BREAK_DISTANCE)
+        {
+            thrust = lerp(0, 100, (float)abs(m_current_state.next_checkpoint_dist) / BREAK_DISTANCE);
+            cerr << "####BREAK distance:" << m_current_state.next_checkpoint_dist << endl;
+        }
+        /*        else if(distance_last_point < BREAK_DISTANCE && abs(next_checkpoint_angle) > CHECK_ANGLE))
+                {
+                    int min_dist = next_checkpoint_dist < distance_last_point ? next_checkpoint_dist : distance_last_point;
+                    if(min_dist > BREAK_DISTANCE)
+                    {
+                        min_dist = BREAK_DISTANCE;
+                    }
+                }
+        */
+        else if (abs(m_current_state.next_checkpoint_angle) > CHECK_ANGLE)
+        {
+            float abs_angle = (float)abs(m_current_state.next_checkpoint_angle);
+            thrust = lerp(100, 0, abs_angle / 90.0f);
+            cerr << "####BREAK angle:" << abs_angle << " thrust: " << thrust << endl;
+        }
+        else
+        {
+            thrust = 100;
+            cerr << "***GOGOGO':" << endl;
+        }
+
+        cerr << " dist:" << m_current_state.next_checkpoint_dist << " dist_LP: " << distance_last_point << endl;
+        cerr << "angle:" << m_current_state.next_checkpoint_angle << " thrust:" << thrust << endl;
+
+        string output;
+
+        if (!m_first_round && !m_boosted && m_checkpoint_index == m_boost_index && thrust == 100 && m_current_state.next_checkpoint_angle < CHECK_ANGLE)
+        {
+            m_boosted = true;
+            output = std::to_string((int)(m_current_state.next_checkpoint_x)) + " " + std::to_string((int)(m_current_state.next_checkpoint_y)) + " BOOST";
+        }
+        else
+        {
+            output = std::to_string((int)(m_current_state.next_checkpoint_x)) + " " + std::to_string((int)(m_current_state.next_checkpoint_y)) + " " + std::to_string(thrust);
+        }
+
+        return output;
+    }
+};
+
+int main()
+{
+    track* m_track = new track();
 
     // game loop
     while (1) {
@@ -55,99 +256,9 @@ int main()
         // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
 
-        if (last_point_x == 0)
-        {
-            last_point_x = x;
-            last_point_y = y;
+        m_track->UpdateStatus(x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle);
 
-            current_point_x = next_checkpoint_x;
-            current_point_y = next_checkpoint_y;
+        cout << m_track->GetOutputString() << endl;
 
-            goals.push_back(pair<int, int>(next_checkpoint_x, next_checkpoint_y));
-        }
-        else if (current_point_x != next_checkpoint_x)
-        {
-            last_point_x = current_point_x;
-            last_point_y = current_point_y;
-
-            current_point_x = next_checkpoint_x;
-            current_point_y = next_checkpoint_y;
-
-            if (!goals.empty() && goals[0].first == next_checkpoint_x)
-            {
-                goals_collected = true;
-            }
-
-            if (!goals_collected)
-            {
-                goals.push_back(pair<int, int>(next_checkpoint_x, next_checkpoint_y));
-            }
-            else if (goals[1].first == next_checkpoint_x)
-            {
-                boosted = false;
-            }
-        }
-
-        float delta_x = (next_checkpoint_x - x);
-        float delta_y = (next_checkpoint_y - y);
-
-        //cerr << "Pos: (" << x << ", " << y << ")" << endl;
-        //cerr << "Checkpoint: (" << next_checkpoint_x << ", " << next_checkpoint_y << ")" << endl;
-        //cerr << "Delta: (" << delta_x << ", " << delta_y << ")" << endl;
-
-        float angle = atan(delta_y / delta_x);
-        float distance = sqrt(delta_y * delta_y + delta_x * delta_x);
-
-        float delta_lastpoint_x = (last_point_x - x);
-        float delta_lastpoint_y = (last_point_y - y);
-        float distance_last_point = sqrt(delta_lastpoint_y * delta_lastpoint_y + delta_lastpoint_x * delta_lastpoint_x);
-
-        float x_multiplier = delta_x < 0.0f ? -1 : 1;
-        float y_multiplier = delta_y < 0.0f ? -1 : 1;
-
-        //cerr << "angle: " <<  angle * 180.0f / PI << "  distance: " << distance << endl;
-        // cerr << "multipliers: (" << x_multiplier << ", " << y_multiplier << ")" << endl;
-        float goal_distance = distance - TARGET_DISTANCE;
-        float dir_x = cos(angle) * goal_distance;
-        float dir_y = sin(angle) * goal_distance;
-        float goal_x = x + (x_multiplier)*abs(dir_x);
-        float goal_y = y + (y_multiplier)*abs(dir_y);
-
-        //cerr << "Goal: (" << goal_x << ", " << goal_y << "), dir: (" << dir_x << ", " << dir_y << ")" << endl;
-
-        if (abs(next_checkpoint_angle) > 90)
-        {
-            cerr << "====STOP:" << endl;
-            thrust = 0;
-        }
-        else if ((next_checkpoint_dist < BREAK_DISTANCE || distance_last_point < BREAK_DISTANCE) && abs(next_checkpoint_angle) > CHECK_ANGLE)
-        {
-            int min_dist = next_checkpoint_dist < distance_last_point ? next_checkpoint_dist : distance_last_point;
-            if (min_dist > BREAK_DISTANCE)
-            {
-                min_dist = BREAK_DISTANCE;
-            }
-            thrust = lerp(100, 0, (float)min_dist / BREAK_DISTANCE);
-            cerr << "####BREAK:" << endl;
-        }
-        else
-        {
-            thrust = 100;
-            cerr << "***GOGOGO':" << endl;
-        }
-
-        cerr << "angle:" << next_checkpoint_angle << " dist:" << next_checkpoint_dist << " distance_last_point" << distance_last_point << " thrust:" << thrust << endl;
-
-        if (!boosted && next_checkpoint_dist > 4000 && thrust == 100 && next_checkpoint_angle == 0)
-        {
-            boosted = true;
-            cout << (int)goal_x << " " << (int)goal_y << " " << "BOOST" << endl;
-        }
-        else
-        {
-            cout << (int)next_checkpoint_x << " " << (int)next_checkpoint_y << " " << thrust << endl;
-        }
-
-        last_distance = next_checkpoint_dist;
     }
 }
