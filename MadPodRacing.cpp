aec4dd308485615824fa2d9126096916104e632e
Christina <chellstr@iki.fi>
2021-12-13 23:33:52 +0200

    Wood League - Initial check in
    - Initial check in
    - Experimenting with different thrusts wrt angle

---
+++ MadPodRacing.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+
+    // game loop
+    while (1) {
+        int x;
+        int y;
+        int next_checkpoint_x; // x position of the next check point
+        int next_checkpoint_y; // y position of the next check point
+        int next_checkpoint_dist; // distance to the next checkpoint
+        int next_checkpoint_angle; // angle between your pod orientation and the direction of the next checkpoint
+        cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
+        int opponent_x;
+        int opponent_y;
+        cin >> opponent_x >> opponent_y; cin.ignore();
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+
+        // You have to output the target position
+        // followed by the power (0 <= thrust <= 100)
+        // i.e.: "x y thrust"
+        cerr << "next_checkpoint_angle:" << next_checkpoint_angle << endl;
+        int thrust = abs(next_checkpoint_angle) > 80 ? 20 : 100;
+        cerr << "thrust:" << thrust << endl;
+        cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << endl;
+    }
+}
